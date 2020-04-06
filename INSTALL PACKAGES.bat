71d2427d361003877efc881032ccfeb6d9afdd83
FENDRA Tools <62420000+fendradev@users.noreply.github.com>
2020-04-07 08:51:15 +1000

    Add files via upload

---
+++ INSTALL PACKAGES.bat
@@ -0,0 +1,10 @@
+@echo off
+ECHO Installing the required packages for the bot!
+TIMEOUT 3
+
+py -3 -m pip install -U -r requirements.txt
+
+ECHO Thanks for Choosing TriStar!
+ECHO Now run START BOT.bat !
+PAUSE
+
